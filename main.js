6d56f2df38f717908d4b16614c8fddfe9cef1eae
Rassel Ahmed <94746949+Rasselahmed@users.noreply.github.com>
2022-02-04 16:54:26 +0600

    Add files via upload
    upload

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+
+
+function rassel() {
+    
+    document.write("ahmed");
+}
+
+
+rassel();
+rassel();
+rassel();
+
+
+function ss() {
+    var a= 20;
+    var b= 30;
+    var total=a+b;
+
+    document.write(total)
+}
+ss();
+ss();
+ss();
